7722a5e87f974b3dd3ebb5e4acc22c61dda30080
Freeman <freeman.hsu@highlight.mobi>
2022-06-13 14:05:10 +0800

    [library] upgrade hilt gradle plugin to 2.42

--- app/src/main/java/idv/hsu/media/downloader/worker/DownloadMediaWorker.kt
+++ app/src/main/java/idv/hsu/media/downloader/worker/DownloadMediaWorker.kt
@@ -110,7 +110,7 @@ class DownloadMediaWorker @AssistedInject constructor(
 
                 repoDownloadRecord.addDownloadRecord(downloadRecord)
 
-                ytdlp.execute(request) { progress, etaInSeconds, line ->
+                ytdlp.execute(request) { progress, etaInSeconds ->
                     updateForegroundDownloadInfo(applicationContext, id, fileName, progress)
                     downloadRecord.downloadProgress = progress
                     downloadRecord.downloadState = if (progress < 0) DOWNLOAD_STATE_PARSING else DOWNLOAD_STATE_DOWNLOADING
